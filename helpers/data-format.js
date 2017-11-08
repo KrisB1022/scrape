c5ff97f9cbaf67743363a096b4c0facc222ab5fd
Kris Byrum <krisjbyrum@gmail.com>
2017-11-08 14:59:08 -0800

    Updates get location to include pagination

--- helpers/data-format.js
+++ helpers/data-format.js
@@ -1,13 +1,15 @@
 const dataFormat = data => {
     const tempArr = []
-    data = data.locations
-    data.filter(i => i).forEach(item => {
-        let tempObj = {}
-        tempObj.location = item[0]
-        tempObj.url = item[1]
-        tempArr.push(tempObj)
-    })
 
+    data.forEach( group => {
+        group.forEach(item => {
+            let tempObj = {}
+            tempObj.location = item[0]
+            tempObj.url = item[1]
+            tempArr.push(tempObj)
+        })
+    })
+    
     return tempArr
 }
 
--- helpers/get-locations.js
+++ helpers/get-locations.js
@@ -1,19 +1,42 @@
 const Nightmare = require('nightmare')
 
+const evaluateLinks = async (nightmare) => {
+    const links = await nightmare
+        .evaluate(() => {
+            return [...document.querySelectorAll('.sitemap ul li a')]
+                .filter(el => el.href.indexOf('?page=') == -1)
+                .map(el => [el.innerText, el.href])
+        })
+    return links
+}
+
 const getLocations = async url => {
     console.log(`Grabbing locations from ${url}`)
-    const nightmare = new Nightmare({ show: false })
+    const nightmare = new Nightmare({ show: true })
+    const tempArr = []
+    let next = true
+    let i = 1
 
     try {
-        const locations = await nightmare
-            .goto(url)
-            .wait('.sitemap')
-            .evaluate(() => {
-                return [...document.querySelectorAll('.sitemap ul li a')]
-                    .map(el => [el.innerText, el.href])
-            })
-            .end()
-        return { locations }
+        await nightmare.goto(url).wait('.sitemap')
+
+        while(next) {
+            await nightmare.wait('.sitemap')
+            console.log('  checking page ...', i)
+            tempArr.push(await evaluateLinks(nightmare))
+            
+            const nextPresent = await nightmare.exists('.sitemap .next a')
+            if(nextPresent && i <= 8) {
+                nightmare.click('.sitemap .next a')
+                i++
+            } else {
+                next = false
+                await nightmare.end()
+            }
+        }
+
+        return tempArr
+        
     } catch (err) {
         console.error(err)
     }
