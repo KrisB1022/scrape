54dd0aa013633da680d10b164e755fc369939ad3
Kris Byrum <krisjbyrum@gmail.com>
2017-11-08 15:19:28 -0800

    Updates pricing reading function

--- get-pricing.js
+++ get-pricing.js
@@ -4,11 +4,11 @@ const { readFileSync, writeFileSync } = require('fs')
 
 const parsePrice = str => str.slice( str.indexOf('$') + 1, str.length - 1 )
 
-const getPricing = async cityName => {
+const getPricing = async (cityName, url) => {
     console.log(`Now checking ${cityName}`)
     cityName = cityName.split(' ').join('-')
-    const START = `https://www.airbnb.com/s/${cityName}/homes`
-    const nightmare = new Nightmare({ show: true })
+    const START = `${url}/homes`
+    const nightmare = new Nightmare({ show: false })
 
     try {
         await nightmare
@@ -35,23 +35,27 @@ const getPricing = async cityName => {
                     .map(el => el.innerText)
             })
             .end()
-        return { cityName, price: parsePrice(price[2]), country: location[0], state: location[1], city: location[2] }
+        return { cityName, price: parsePrice(price[2]), country: location[0], state: location[1] || 'na', city: location[2] || 'na' }
     } catch (e) {
         console.error(e)
         return undefined
     }
 }
 
-const locations = ['Seattle', 'Tacoma', 'New York']
+const locations = readFileSync('./sitemap/countries/Aruba/Aruba.csv', { encoding: 'utf8' }).trim().split('\n')
+locations.shift()
 
-const series = locations.reduce(async (queue, location) => {
+const series = locations.reduce(async (queue, location) => {   
     const dataArr = await queue
-    dataArr.push(await getPricing(location))
+    temp = location.split(',')
+    dataArr.push(await getPricing(temp[0], temp[1]))
     return dataArr
 }, Promise.resolve([]))
 
 series.then(data => {
-    const csvData = csvFormat(data.filter(i => i))
-    writeFileSync('./prices.csv', csvData, { encoding: 'utf8' })
+    if(data) {
+        const csvData = csvFormat(data.filter(i => i))
+        writeFileSync('./prices.csv', csvData, { encoding: 'utf8' })
+    }
 })
 .catch(e => console.error(e))
